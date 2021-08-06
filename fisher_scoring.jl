350636c487fc93593b8a11dbec09e021d5ccaca0
julstpierre <stpierre.ju@gmail.com>
2021-08-06 15:50:49 -0400

    Small updates

--- fisher_scoring.jl
+++ fisher_scoring.jl
@@ -40,7 +40,7 @@ hessian(x::Matrix{Float64},
         dist.hessian_function(x, beta)
 
 # Implement Fisher scoring to find MLE of beta
-function Fisher_scoring(y::Vector{Float64}, x::Matrix{Float64}, dist::Distribution; beta::Vector{Float64}=[0.0, 0.0], tol=0.01)
+function Fisher_scoring(y::Vector{Float64}, x::Matrix{Float64}, dist::Distribution, beta::Vector{Float64}=[0.0, 0.0], tol=0.01)
     """
     Some nice documentation here.
     """
@@ -61,6 +61,6 @@ x = [6.1, 4.2, 0.5, 8.8, 1.5, 9.2, 8.5, 8.7, 6.7, 6.5, 6.3, 6.7, 0.2, 8.7, 7.5]
 y = [0.8, 3.5, 12.4, 1.1, 8.9, 2.4, 0.1, 0.4, 3.5, 8.3, 2.6, 1.5, 16.6, 0.1, 1.3]
 
 # Find MLEs and SE for beta0 and beta1
-beta_hat, beta_se = Fisher_scoring(y, hcat(ones(length(x)), x), exponential, [1.0, 1])
+beta_hat, beta_se = Fisher_scoring(y, hcat(ones(length(x)), x), exponential, [1000.0, 1000])
 beta_ci = round.([beta_hat - 1.96*beta_se beta_hat + 1.96*beta_se], digits = 3)
 beta0_ci, beta1_ci  = beta_ci[1,:], beta_ci[2,:]
